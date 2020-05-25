57006f669469b59c55cd585ac41d990e5d85e1ef
cftcn <58070700+cftcn@users.noreply.github.com>
2020-05-25 17:00:08 +0800

    testinit&prt

--- main.c
+++ main.c
@@ -1,5 +1,34 @@
 #include<stdio.h>
+#include<pthread.h>
+#define SIZE 100000
+
+void testinit(int a[]);//生成数组
+void prt(int a[]);//打印数组
 int main()
 {
-    
+    //test程序
+    int raw[SIZE];
+    testinit(raw);
+    prt(raw);
+}
+
+void testinit(int a[])
+{
+    for(int i = 1; i <= SIZE; ++i) a[i] = rand()%(SIZE+1);
+}
+
+void prt(int a[])
+{
+    int count = 0;
+    for(int i = 1; i <= SIZE; ++i)
+    {
+        printf("%10d ", a[i]);
+        ++count;
+        if(count == 10)
+        {
+            printf("\n");
+            count = 0;
+        }
+    }
+    printf("\n");
 }
